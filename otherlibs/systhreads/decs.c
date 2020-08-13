79b149c35e348669317826e0f4e8a5288707bfbf
Enguerrand Decorne <decorne.en@gmail.com>
2020-08-13 02:16:57 +0200

    lama

--- otherlibs/systhreads/decs.c
+++ otherlibs/systhreads/decs.c
@@ -135,11 +135,13 @@ static void caml_dec_main_lock_acquire(struct caml_dec_main_lock *m)
     atomic_fetch_add(&m->waiters, -1);
   }
   atomic_store_rel(&m->busy, 1);
-  caml_bt_acquire_domain_lock();
-  if (atomic_load_acq(&m->backup_thread_on)) {
-    atomic_store_rel(&m->backup_thread_on, 0);
-    caml_bt_enter_ocaml();
-  }
+  if (!caml_domain_alone()) {
+    caml_bt_acquire_domain_lock();
+    if (atomic_load_acq(&m->backup_thread_on)) {
+      atomic_store_rel(&m->backup_thread_on, 0);
+      caml_bt_enter_ocaml();
+    }
+  };
   caml_plat_unlock(&m->lock);
 }
 
@@ -149,12 +151,14 @@ static void caml_dec_main_lock_release(struct caml_dec_main_lock * m)
   atomic_store_rel(&m->busy, 0);
   // if busy = 0, it means no thread was running code
   // thus we need to notify the backup thread.
-  caml_bt_release_domain_lock();
-  if (atomic_load_acq(&m->waiters) == 0 &&
-      atomic_load_acq(&m->backup_thread_on) == 0) {
-    atomic_store_rel(&m->backup_thread_on, 1);
-    caml_bt_exit_ocaml();
-  }
+  if (!caml_domain_alone()) {
+    caml_bt_release_domain_lock();
+    if (atomic_load_acq(&m->waiters) == 0 &&
+	atomic_load_acq(&m->backup_thread_on) == 0) {
+      atomic_store_rel(&m->backup_thread_on, 1);
+      caml_bt_exit_ocaml();
+    }
+  };
   caml_plat_signal(&m->free);
   caml_plat_unlock(&m->lock);
 }
@@ -342,7 +346,8 @@ static void caml_dec_reinitialize(void)
   All_decs = Current_dec;
 
   caml_dec_main_lock_init(&Dec_main_lock);
-  caml_bt_acquire_domain_lock();
+  if (!caml_domain_alone())
+    caml_bt_acquire_domain_lock();
   caml_dec_main_lock_release(&Dec_main_lock);
 }
 
