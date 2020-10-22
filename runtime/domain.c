a93104224efd7b78e0c87d1483248f84e6b623bf
Enguerrand Decorne <decorne.en@gmail.com>
2020-10-22 17:28:50 +0200

    runtime: allow OCAMLRUNPARAM=s to override Minor_heap_max to set arbirary big minor heap sIZE

--- runtime/domain.c
+++ runtime/domain.c
@@ -113,6 +113,7 @@ static struct dom_internal all_domains[Max_domains];
 
 CAMLexport atomic_uintnat caml_num_domains_running;
 
+static uintnat minor_heaps_max_size;
 CAMLexport uintnat caml_minor_heaps_base;
 CAMLexport uintnat caml_minor_heaps_end;
 static __thread dom_internal* domain_self;
@@ -157,8 +158,8 @@ asize_t caml_norm_minor_heap_size (intnat wsize)
   if (wsize < Minor_heap_min) wsize = Minor_heap_min;
   bs = caml_mem_round_up_pages(Bsize_wsize (wsize));
 
-  Assert(page_size * 2 < Minor_heap_max);
-  max = Minor_heap_max - page_size * 2;
+  Assert(page_size * 2 < minor_heaps_max_size);
+  max = minor_heaps_max_size - page_size * 2;
 
   if (bs > max) bs = max;
 
@@ -327,20 +328,32 @@ CAMLexport void caml_reset_domain_lock(void)
 void caml_init_domains(uintnat minor_heap_wsz) {
   int i;
   uintnat size;
+  uintnat heaps_max_size;
   void* heaps_base;
 
-  /* sanity check configuration */
-  if (caml_mem_round_up_pages(Minor_heap_max) != Minor_heap_max)
-    caml_fatal_error("Minor_heap_max misconfigured for this platform");
+  /* Minor_heap_max can be overriden with OCAMLRUNPARAM if s > Minor_heap_max */
+  /* We also need to check in any case that everything is properly aligned   */
+  /* to page boundary. */
+  if (Minor_heap_max < minor_heap_wsz) {
+    if (caml_mem_round_up_pages(minor_heap_wsz) != minor_heap_wsz)
+      caml_fatal_error("Configured minor heap size is not properly aligned");
+    heaps_max_size = minor_heap_wsz;
+  }
+  else {
+    if (caml_mem_round_up_pages(Minor_heap_max) != Minor_heap_max)
+      caml_fatal_error("Minor misconfigured for this platform");
+    heaps_max_size = Minor_heap_max;
+  };
 
   /* reserve memory space for minor heaps */
-  size = (uintnat)Minor_heap_max * Max_domains;
+  size = (uintnat)heaps_max_size * Max_domains;
 
   heaps_base = caml_mem_map(size*2, size*2, 1 /* reserve_only */);
   if (!heaps_base) caml_raise_out_of_memory();
 
   caml_minor_heaps_base = (uintnat) heaps_base;
   caml_minor_heaps_end = (uintnat) heaps_base + size;
+  minor_heaps_max_size = heaps_max_size;
 
   for (i = 0; i < Max_domains; i++) {
     struct dom_internal* dom = &all_domains[i];
@@ -361,7 +374,7 @@ void caml_init_domains(uintnat minor_heap_wsz) {
     dom->backup_thread_msg = BT_INIT;
 
     domain_minor_heap_base = caml_minor_heaps_base +
-      (uintnat)Minor_heap_max * (uintnat)i;
+      minor_heaps_max_size * (uintnat)i;
     dom->tls_area = domain_minor_heap_base;
     dom->tls_area_end =
       caml_mem_round_up_pages(dom->tls_area +
@@ -369,7 +382,7 @@ void caml_init_domains(uintnat minor_heap_wsz) {
     dom->minor_heap_area = /* skip guard page */
       caml_mem_round_up_pages(dom->tls_area_end + 1);
     dom->minor_heap_area_end =
-      domain_minor_heap_base + Minor_heap_max;
+      domain_minor_heap_base + minor_heaps_max_size;
   }
 
 
@@ -578,7 +591,7 @@ struct domain* caml_domain_self()
 struct domain* caml_owner_of_young_block(value v) {
   Assert(Is_young(v));
   int heap_id = ((uintnat)v - caml_minor_heaps_base) /
-    Minor_heap_max;
+    minor_heaps_max_size;
   return &all_domains[heap_id].state;
 }
 
