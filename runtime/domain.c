643037e7260dcbb98dbe87da98740e117e44ae52
Enguerrand Decorne <decorne.en@gmail.com>
2021-06-10 18:35:46 +0200

    runtime: reintroduce adjust_gc_speed

--- runtime/domain.c
+++ runtime/domain.c
@@ -239,6 +239,8 @@ static void create_domain(uintnat initial_minor_heap_wsize) {
     domain_state->unique_id = d->interruptor.unique_id;
     d->state = domain_state;
 
+    domain_state->extra_heap_resources = 0.0;
+
     if (caml_init_signal_stack() < 0) {
       goto init_signal_stack_failure;
     }
